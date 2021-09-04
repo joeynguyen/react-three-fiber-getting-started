c81fe0c315d92314e6634b9db0410cc888f527d3
Joey Nguyen <jnn193@gmail.com>
2021-09-04 12:51:50 -0500

    create a spinning cube

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
-import { Canvas } from '@react-three/fiber';
+import { useRef } from 'react';
+import { Canvas, useFrame } from '@react-three/fiber';
 import './App.scss';
+
+function Box() {
+  const mesh = useRef(null);
+
+  // useFrame can't be used inside App component (or the component being displayed)
+  // directly so we have move it to separate component
+  useFrame(() => {mesh.current.rotation.x = mesh.current.rotation.y += 0.01});
+
+  return (
+    <mesh ref={mesh}>
+      <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
+      <meshStandardMaterial attach="material" />
+    </mesh>
+  );
+}
 
 function App() {
   return (
     <>
       <Canvas>
-        <mesh>
-          <boxBufferGeometry attach="geometry" args={[1,1,1]} />
-          <meshStandardMaterial attach="material" />
-        </mesh>
-
+        <Box />
       </Canvas>
     </>
   );
