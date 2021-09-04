763d2eb090a98c93c56cd8f4172cd886ab178944
Joey Nguyen <jnn193@gmail.com>
2021-09-04 13:07:55 -0500

    use props to change each SpinningMesh component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { useRef } from 'react';
 import { Canvas, useFrame } from '@react-three/fiber';
 import './App.scss';
 
-function Box() {
+function SpinningMesh({ position, color, args }) {
   const mesh = useRef(null);
 
   // useFrame can't be used inside App component (or the component being displayed)
@@ -10,9 +10,9 @@ function Box() {
   useFrame(() => {mesh.current.rotation.x = mesh.current.rotation.y += 0.01});
 
   return (
-    <mesh ref={mesh}>
-      <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
-      <meshStandardMaterial attach="material" color="lightblue" />
+    <mesh position={position} ref={mesh}>
+      <boxBufferGeometry attach="geometry" args={args} />
+      <meshStandardMaterial attach="material" color={color} />
     </mesh>
   );
 }
@@ -20,9 +20,11 @@ function Box() {
 function App() {
   return (
     <>
-      <Canvas camera={{ position: [-5, 2, 10], fov: 20}}>
+      <Canvas camera={{ position: [-5, 2, 10], fov: 60}}>
         <ambientLight intensity={0.3} />
-        <Box />
+        <SpinningMesh position={[0, 1, 0]} color="lightblue" args={[3, 2, 1]} />
+        <SpinningMesh position={[-2, 1, -5]} color="pink" />
+        <SpinningMesh position={[5, 1, -2]} color="pink" />
       </Canvas>
     </>
   );
